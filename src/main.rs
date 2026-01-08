51cc0be89a4ed09e63a0aadc863c4c9699f5af3a
Sezer Aptourachman (julsezar) <sezeraptourachman@gmail.com>
2026-01-09 01:28:49 +0300

    Fixed some bugs, added text animation to the wizard

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,63 @@
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
+use fancy_print::{FancyPrinter, Animation};
+use std::time::Duration;
+
+fn wizard_speak(text: &str) {
+    let printer = FancyPrinter::builder()
+        .animation(Animation::CharacterCycling)
+        .time_delay(Duration::from_millis(1))
+        .multi_line(false)
+        .build();
+
+    printer.print(text);
+    println!();
+}
 
 fn main() {
-    println!("Welcome to Guess the Number!");
-    println!("I am Marvolo the wizard!");
-    println!("You can't see me, but I can see you...");
+    wizard_speak("Welcome to Guess the Number!");
+    wizard_speak("I am Marvolo the wizard!");
+    wizard_speak("You can't see me, but I can see you...");
+
     println!("Press Enter to continue...");
     io::stdin().read_line(&mut String::new()).expect("Failed to read input");
-    println!("Nobody ever succeeded in defeating me!");
-    println!("Select your difficulty and let chaos begin!");
+
+    wizard_speak("Nobody ever succeeded in defeating me!");
+    wizard_speak("Select your difficulty and let chaos begin!");
 
     let difficulty = select_difficulty();
     let config = create_game_config(difficulty);
 
-    println!("I'm thinking of a number between 1 and {}", config.max_number);
+    wizard_speak(&format!("I'm thinking of a number between 1 and {}", config.max_number));
 
     let secret_number = rand::thread_rng().gen_range(1..=config.max_number);
     let mut attempts = 0;
 
     loop {
-        if attempts >= config.max_attempts {
-            println!("Game over! You lost!");
-            println!("The number was {}", secret_number);
-            break;
-        }
-
         println!("Attempts left: {}", config.max_attempts - attempts);
 
         let guess = read_guess();
         attempts += 1;
 
+        if attempts >= config.max_attempts {
+            wizard_speak("Game over! You lost!");
+            wizard_speak(&format!("The number was {}", secret_number));
+            wizard_speak("Ahahahaha! You're a loser, cannot even guess my secret number with the hints I give. I WIN!!!");
+            break;
+        }
+
         match guess.cmp(&secret_number) {
             Ordering::Less => {
-                println!("You're low, try a higher number!");
-                print_hint(guess, secret_number);
+                wizard_speak("You're low, try a higher number!");
+                print_hint(guess, secret_number, config.max_number);
             },
             Ordering::Greater => {
-                println!("You're high, try a lower number!");
-                print_hint(guess, secret_number);
+                wizard_speak("You're high, try a lower number!");
+                print_hint(guess, secret_number, config.max_number);
             },
             Ordering::Equal => {
-                println!("Wow! You really guessed my secret number.
-                    Are you a secret wizard or something?");
+                wizard_speak("Wow! You really guessed my secret number. Are you a secret wizard or something?");
                 print_achievement(attempts);
                 break;
             }
@@ -50,23 +65,29 @@ fn main() {
     }
 }
 
-fn print_hint(guess: u32, secret: u32) {
+fn print_hint(guess: u32, secret: u32, max_number: u32) {
     let difference = (guess as i32 - secret as i32).abs();
-    match difference {
-        1..=5 => println!("You're very near to the end folk!"),
-        6..=25 => println!("You're closer, but not that much!"),
-        26..=50 => println!("Actually you're quite in the range of my number!"),
-        _ => println!("You, loser! FAR AWAY!!!"),
+
+    let closeness = (difference as f32 / max_number as f32) * 100.0;
+
+    if closeness <= 3.0 {
+        wizard_speak("You're very near to the end folk!")
+    } else if closeness <= 15.0 {
+        wizard_speak("You're closer, but not that much!")
+    } else if closeness <= 25.0 {
+        wizard_speak("Actually you're quite in the range of my number!")
+    } else {
+        wizard_speak("You, loser! FAR AWAY!!!")
     }
 }
 
 fn print_achievement(attempts: u32) {
     match attempts {
-        1 => println!("Oh, no! You defeated my game. What an idiot! No, this...
+        1 => wizard_speak("Oh, no! You defeated my game. What an idiot! No, this...
                         Cannot happen..."),
-        2..=3 => println!("Well done boy, you defeated me too soon. I knew this was gonna happen..."),
-        4..=5 => println!("Actually, not bad. But you can ascend more!"),
-        _ => println!("Ahahahaha! You're a loser, cannot even guess my secret number with the hints I give. I WIN!!!")
+        2..=3 => wizard_speak("Well done boy, you defeated me too soon. I knew this was gonna happen..."),
+        4..=5 => wizard_speak("Actually, not bad. But you can ascend more!"),
+        _ => wizard_speak("You barely made it, but a win is a win, I suppose..."),
     }
 }
 
